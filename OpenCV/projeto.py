40efecfd855df55390f44ec4c9d096a89649ec43
Toni Silva <baianodeposito9@gmail.com>
2026-05-17 18:06:02 -0300

    Update projeto.py

--- OpenCV/projeto.py
+++ OpenCV/projeto.py
@@ -1,3 +1,33 @@
-from cv2 import * 
-import cv2 as app
-camera = app.cv2.Vi
+import cv2
+
+camera = cv2.VideoCapture(0)
+
+detector = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+
+while True:
+    sucesso, frame = camera.read()
+
+    cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    rostos = detector.detectMultiScale(
+        cinza,
+        scaleFactor=1.3,
+        minNeighbors=5
+    )
+
+    for(x, y, largura, altura) in rostos:
+        cv2.rectangle(
+            frame, 
+            (x, y),
+            (x + largura, y + altura),
+            (255, 0, 0),
+            2
+        )
+    cv2.imshow("Deteccao Facial", frame)
+    if cv2.waitKey(1) == 27:
+        break
+
+camera.release()
+cv2.destroyAllWindows()
